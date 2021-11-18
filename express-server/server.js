93a73ab5694f255fe0545f2c621aaf693ecdc214
CarlaOnate <carlaonategardella@gmail.com>
2021-11-17 20:20:58 -0600

    added js functions to add to freeboard but were tested there

--- express-server/server.js
+++ express-server/server.js
@@ -7,9 +7,21 @@ app.use(cors())
 const PORT = 8000;
 
 const jsonDB = require('./test_freeboard.json')
-console.log("jsonDB", jsonDB)
 
+const locationTest = () => {
+    //Worked perfectly
+    const location = "19.283093207526942, -99.13738877457311"
+    const split = location.split(', ')
+    const lat = split[0]
+    const long = split[1]
+    console.log(lat, long)
+}
 
+const allLocationsTest = () => {
+    //Didn't work because freeboard does not receive array of values. Only receives one string
+    const lats = jsonDB.map(el => el.ubicacion.split(', ')[0])
+    console.log(lats)
+}
 
 app.get('/', cors() ,function(req,res){
     console.log("req /json")
