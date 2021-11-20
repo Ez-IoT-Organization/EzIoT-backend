99e1e92b193050e2d81d03424799f18aa897ff4f
CarlaOnate <carlaonategardella@gmail.com>
2021-11-20 14:14:16 -0600

    Default dashboard works

--- express-server/server.js
+++ express-server/server.js
@@ -7,15 +7,18 @@ app.use(cors())
 const PORT = 8000;
 
 const jsonDB = require('./test_freeboard.json')
+console.log(jsonDB["resultset"]["row"][0]["field"][1]["$t"])
 
 const locationTest = () => {
     //Worked perfectly
-    const location = "19.283093207526942, -99.13738877457311"
-    const split = location.split(', ')
-    const lat = split[0]
-    const long = split[1]
-    console.log(lat, long)
+    const location = jsonDB["resultset"]["row"][0]["field"][1]["$t"]
+    const split1 = location.split(' | ')
+    const lat = location.split(' | ')[0].split('x=')[1]
+    const long = location.split(' | ')[1].split('y=')[1]
+    const test = location.split(' | ')[1].split('y=')[1]
+    console.log("lat y long", split1, lat, long, test)
 }
+locationTest()
 
 const allLocationsTest = () => {
     //Didn't work because freeboard does not receive array of values. Only receives one string
