b484ae69321ee771568f6a5025ba555f274cc25f
justNeto <49221536+justNeto@users.noreply.github.com>
2021-11-29 19:16:57 -0600

    Update distance.py

--- RaspImp/distance.py
+++ RaspImp/distance.py
@@ -1,1 +1,15 @@
-print("Hello World")
+import warnings
+from gpiozero import DistanceSencor
+from time import sleep
+from pythonosc import osc_message_builder
+from pythonosc import udp_client
+
+warnings.filterwarnings("ignore")
+
+sensor = DistanceSensor(echo = 17, trigger = 4)
+sender = udp_client.SimpleUDPClient('127.0.0.1', 4559)
+
+x = sensor.distance
+print(round(x,3))
+pitch = round(x*100+30)
+sender.send_message('/play_this', pitch)
